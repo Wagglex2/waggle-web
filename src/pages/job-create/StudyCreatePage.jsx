6a6dd8b51c1d49bd6ac162950cf418efd2ef5b02
JJyen <129050370+JJyen@users.noreply.github.com>
2025-09-23 18:03:18 +0900

    fix: 누락된 코드 재작성 및 추가 (#6)

--- src/pages/job-create/StudyCreatePage.jsx
+++ src/pages/job-create/StudyCreatePage.jsx
@@ -1,9 +1,33 @@
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
+import InputcheckList from '@/features/job-create/components/InputCheckList';
+import StudyCreateForm from '@/features/job-create/StudyCreateForm';
+
 const StudyCreatePage = () => {
   return (
-    <div>
-      <div>스터디 공고 등록하기</div>
+    <div css={studyCreateContainer}>
+      <header>[스터디] 공고 작성</header>
+      <main>
+        <StudyCreateForm />
+        <InputcheckList />
+      </main>
     </div>
   );
 };
 
 export default StudyCreatePage;
+
+const studyCreateContainer = css`
+  font-family: 'nanumR';
+  header {
+    font-size: 24px;
+    font-family: 'nanumEB';
+    width: 900px;
+    margin: 60px auto;
+  }
+
+  main {
+    display: flex;
+    justify-content: center;
+  }
+`;
