b59a8064c36816fe9eee566476977dc115b2fdd0
minji990 <rlaalswl6749@gamil.com>
2025-11-04 14:10:38 +0900

    feat: 로그인 ui 생성(ballpit)

--- src/pages/SigninPage.jsx
+++ src/pages/SigninPage.jsx
@@ -1,9 +1,51 @@
-const SigninPage = () => {
+import React from 'react';
+import BallpitBackground from '../components/BallpitBackground';
+
+const ballColors = [
+  0xfcd514, // 노랑
+  0xfffeef, // 아이보리
+  0xf7a41a, // 주황
+];
+
+function SigninPage() {
   return (
-    <div>
-      <div>로그인</div>
+    <div
+      style={{
+        position: 'relative',
+        width: '100vw',
+        height: '100vh',
+        background: '#000000',
+      }}
+    >
+      <div
+        style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%', zIndex: 1 }}
+      >
+        <BallpitBackground
+          count={150}
+          gravity={0}
+          friction={0.99}
+          wallBounce={0.95}
+          followCursor={false}
+          colors={ballColors}
+        />
+      </div>
+
+      <div
+        style={{
+          position: 'relative',
+          zIndex: 2,
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          height: '100%',
+        }}
+      >
+        <h1 style={{ color: '#ffffff', textShadow: '1px 1px 2px rgba(0,0,0,0.1)' }}>
+          로그인 페이지
+        </h1>
+      </div>
     </div>
   );
-};
+}
 
 export default SigninPage;
