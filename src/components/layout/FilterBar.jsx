60017f7b51514dcf9e9a6a3d2f263a157fa3e38b
김나은 <migon0258@gmail.com>
2025-11-04 11:05:55 +0900

    refactor: 필터 영역 레이아웃 컴포넌트 분리

---
+++ src/components/layout/FilterBar.jsx
@@ -0,0 +1,47 @@
+/** @jsxRuntime automatic */
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
+import { colors } from "@/styles/theme";
+import ToggleSwitch from "@/components/common/ToggleSwitch";
+
+const filterSectionStyle = css`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 24px;
+  height: 40px;
+`;
+
+const dropDownsContainerStyle = css`
+  display: flex;
+  gap: 10px;
+  align-items: center;
+`;
+
+const toggleContainerStyle = css`
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  gap: 8px;
+  margin-bottom: -30px;
+`;
+
+const toggleTextStyle = css`
+  color: ${colors.gray[300]};
+  font-size: 14px;
+  font-family: 'nanumR';
+`;
+
+export default function FilterBar({ children }) {
+  return (
+    <div css={filterSectionStyle}>
+      <div css={dropDownsContainerStyle}>
+        {children}
+      </div>
+      <div css={toggleContainerStyle}>
+        <span css={toggleTextStyle}>마감된 공고만</span>
+        <ToggleSwitch />
+      </div>
+    </div>
+  );
+}
