cd6d899a0adbb9829b8c8ef3c18b0628a10d128c
JJyen <gdb1632@naver.com>
2025-11-11 18:51:55 +0900

    feat: 마이페이지 사이드바 선택 시 ui 추가

--- src/components/layout/SideBar.jsx
+++ src/components/layout/SideBar.jsx
@@ -1,26 +1,49 @@
 /** @jsxImportSource @emotion/react */
 import { css } from '@emotion/react';
 import { colors } from '../../styles/theme';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 const SideBar = () => {
+  const location = useLocation();
+  const lastPath = location.pathname.split('/').filter(Boolean).pop();
+
+  console.log(lastPath); // "my-posted-job"
+
+  const sidebarList = [
+    {
+      path: '/my-page/profile',
+      name: '내 프로필',
+      current: 'profile',
+    },
+    {
+      path: '/my-page/saved-job',
+      name: '찜한 공고',
+      current: 'saved-job',
+    },
+    {
+      path: '/my-page/my-posted-job',
+      name: '내가 올린 공고',
+      current: 'my-posted-job',
+    },
+    {
+      path: '/my-page/my-applications',
+      name: '내가 지원한 공고',
+      current: 'my-applications',
+    },
+    {
+      path: '/my-page/my-team',
+      name: '내 팀 관리',
+      current: 'my-team',
+    },
+  ];
+
   return (
     <div css={sideBarListBox}>
-      <Link to={'/my-page/profile'}>
-        <p>회원정보</p>
-      </Link>
-      <Link to={'/my-page/saved-job'}>
-        <p>찜한 공고</p>
-      </Link>
-      <Link to={'/my-page/my-posted-job'}>
-        <p>내가 올린 공고</p>
-      </Link>
-      <Link to={'/my-page/my-applications'}>
-        <p>지원한 공고</p>
-      </Link>
-      <Link to={'/my-page/my-team'}>
-        <p>내 팀 관리</p>
-      </Link>
+      {sidebarList.map((item) => (
+        <Link to={item.path}>
+          <p css={currentCategory(item.current, lastPath)}>{item.name}</p>
+        </Link>
+      ))}
     </div>
   );
 };
@@ -32,18 +55,19 @@ const sideBarListBox = css`
   height: 100vh;
   border-right: 1px solid ${colors.gray[100]};
   float: left;
+`;
 
-  p {
-    width: 100%;
-    line-height: 55px;
-    padding-left: 32px;
-    color: ${colors.gray[300]};
-    font-size: 14px;
-    font-family: 'nanumB';
+const currentCategory = (current, lastPath) => css`
+  width: 100%;
+  line-height: 55px;
+  padding-left: 32px;
+  font-size: 14px;
+  font-family: 'nanumB';
+  background-color: ${current === lastPath ? '#fffceb' : '#ffffff'};
+  color: ${current === lastPath ? 'black' : colors.gray[300]};
 
-    &:hover {
-      background-color: #fffceb;
-      color: ${colors.secondary};
-    }
+  &:hover {
+    background-color: #fffceb;
+    color: ${colors.secondary};
   }
 `;
