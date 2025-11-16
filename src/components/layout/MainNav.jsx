63ffb628a9d179c2525c7c6b65013eb93016b69e
JJyen <gdb1632@naver.com>
2025-11-17 02:01:24 +0900

    feat: 네비게이션바 카테고리 선택 상태  ui 추가

--- src/components/layout/MainNav.jsx
+++ src/components/layout/MainNav.jsx
@@ -1,21 +1,47 @@
 /** @jsxImportSource @emotion/react */
 import { css } from '@emotion/react';
 import { colors } from '../../styles/theme';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
+import beeImg from '../../assets/img/nav/bee.png';
+import beehiveImg from '../../assets/img/nav/beehive.png';
+import honeyImg from '../../assets/img/nav/honey.png';
 
 const MainNav = () => {
+  const location = useLocation();
+  const rootPath = location.pathname.split('/').filter(Boolean)[0];
+
+  const navbarList = [
+    {
+      path: '/project-list',
+      name: '프로젝트',
+      icon: beeImg,
+      root: 'project-list',
+    },
+    {
+      path: '/hw-list',
+      name: '과제',
+      icon: honeyImg,
+      root: 'hw-list',
+    },
+    {
+      path: '/study-list',
+      name: '스터디',
+      icon: beehiveImg,
+      root: 'study-list',
+    },
+  ];
+
   return (
     <div css={container}>
       <div css={navBox}>
-        <Link to={'/project-list'}>
-          <p>프로젝트</p>
-        </Link>
-        <Link to={'/hw-list'}>
-          <p>과제</p>
-        </Link>
-        <Link to={'/study-list'}>
-          <p>스터디</p>
-        </Link>
+        {navbarList.map((item) => (
+          <Link key={item.name} to={item.path}>
+            <p css={category(rootPath, item.root)}>
+              <img src={item.icon} />
+              {item.name}
+            </p>
+          </Link>
+        ))}
       </div>
       <button css={createBtn}>공고등록하기</button>
     </div>
@@ -37,10 +63,25 @@ const container = css`
 const navBox = css`
   display: flex;
   margin-top: 5px;
+`;
 
-  p {
-    font-size: 15px;
-    margin-right: 50px;
+const category = (rootPath, root) => css`
+  font-size: 15px;
+  margin-right: 50px;
+  padding-right: 2px;
+  display: flex;
+  justify-content: center;
+  box-shadow: ${rootPath === root ? `inset 0px -3px ${colors.primary}` : 'none'};
+
+  &:hover {
+    box-shadow: inset 0px -3px ${colors.primary};
+  }
+
+  img {
+    width: 23px;
+    height: 23px;
+    margin-right: 3px;
+    margin-left: 1px;
   }
 `;
 
