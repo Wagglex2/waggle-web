5d82a478482051fc70ac2bc4efd509a6670c027d
JJyen <gdb1632@naver.com>
2025-11-26 20:48:08 +0900

    feat: 과제 공고 지원하기 api 연결

--- src/pages/job-detail/components/application-form/HomeworkApplicationForm.jsx
+++ src/pages/job-detail/components/application-form/HomeworkApplicationForm.jsx
@@ -5,8 +5,25 @@ import MyGrade from './application-form-fields/MyGrade';
 import PossibleMethod from './application-form-fields/PossibleMethod';
 import { applicationForm } from './applicationFormStyle';
 import ApplyBtn from './ApplyBtn';
+import useApplicationFormStore from '@/stores/apply/useApplicationFormStore';
+import { useEffect } from 'react';
 
 const HomeworkApplicationForm = () => {
+  const { meetingType, grade, description, reset } = useApplicationFormStore();
+
+  const payload = {
+    category: 'ASSIGNMENT',
+    meetingType: meetingType ?? null,
+    grade: grade?.name ?? null,
+    content: description?.trim() ?? '',
+  };
+
+  useEffect(() => {
+    return () => {
+      reset();
+    };
+  }, []);
+
   return (
     <form css={applicationForm}>
       <h4 className="application-title text-green">[과제] 지원서</h4>
@@ -16,7 +33,11 @@ const HomeworkApplicationForm = () => {
 
       <div>
         <p css={warningMsgStyle}>※본인이 수강하는 과목이 맞는지 반드시 확인하세요 </p>
-        <ApplyBtn />
+        <ApplyBtn
+          isEnabled={meetingType !== null && grade !== null && description.trim() !== ''}
+          data={payload}
+          reset={reset}
+        />
       </div>
     </form>
   );
