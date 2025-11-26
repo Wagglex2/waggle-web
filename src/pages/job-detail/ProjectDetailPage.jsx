7416466d9d6e17aa77fc05cafc3460d02b01837d
JJyen <gdb1632@naver.com>
2025-11-26 14:36:41 +0900

    feat: 프로젝트 공고 상세 보기 api 연결

--- src/pages/job-detail/ProjectDetailPage.jsx
+++ src/pages/job-detail/ProjectDetailPage.jsx
@@ -3,46 +3,98 @@ import { wrap, mainContent } from './jobDetailStyle';
 import ProjectInfo from './components/job-info/ProjectInfo';
 import JobListLinkBtn from './components/JobListLinkBtn';
 import ProjectApplicationForm from './components/application-form/ProjectApplicationForm';
-
-const positions = [
-  {
-    name: 'FRONTEND',
-    desc: '프론트엔드',
-    maxParticipants: 3,
-    currentParticipants: 1,
-  },
-  {
-    name: 'BACKEND',
-    desc: '백엔드',
-    maxParticipants: 2,
-    currentParticipants: 2,
-  },
-  {
-    name: 'DESIGNER',
-    desc: '디자이너',
-    maxParticipants: 2,
-    currentParticipants: 1,
-  },
-  {
-    name: 'PM',
-    desc: 'PM',
-    maxParticipants: 1,
-    currentParticipants: 1,
-  },
-  {
-    name: 'MOBILE',
-    desc: '모바일',
-    maxParticipants: 2,
-    currentParticipants: 0,
-  },
-];
+import { useParams } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import api from '@/api/api';
 
 const ProjectDetailPage = () => {
+  const params = useParams();
+  const defaultImgUrl =
+    'https://waggle-image-bucket.s3.ap-northeast-2.amazonaws.com/user-profile-images/default-profile-image.png';
+
+  // 공고 기본 데이터
+  const [metaData, setMataData] = useState({
+    imgUrl: defaultImgUrl,
+    recruiterId: -1,
+    recruiterNickname: '',
+    viewCount: 0,
+    title: '',
+    createdAt: '0000-00-00',
+    deadline: '0000-00-00',
+    recruitmentId: -1,
+  });
+  const [bookMarkState, setBookMarkState] = useState({
+    isChecked: false,
+    id: -1,
+  });
+
+  // 공고 요약
+  const [summary, setSummary] = useState({
+    purpose: '',
+    meetingType: '',
+    grades: [],
+    startDate: '0000-00-00',
+    endDate: '0000-00-00',
+    positions: [],
+    techStack: [],
+  });
+
+  // 공고 상세
+  const [projectDetail, setProjectDetail] = useState('');
+
+  useEffect(() => {
+    async function getProjectInfo() {
+      try {
+        const res = await api.get(`/api/v1/projects/${params.id}`);
+        console.log(res);
+        const projectInfo = res.data.data;
+        setMataData({
+          imgUrl: defaultImgUrl,
+          recruiterId: projectInfo.authorId,
+          recruiterNickname: projectInfo.authorNickname,
+          viewCount: projectInfo.viewCount,
+          title: projectInfo.title,
+          createdAt: projectInfo.createdAt,
+          deadline: projectInfo.deadline,
+          recruitmentId: projectInfo.id,
+        });
+
+        setBookMarkState({
+          isChecked: projectInfo.bookmarked,
+          id: projectInfo.bookmarkId ?? -1,
+        });
+
+        setSummary({
+          purpose: projectInfo.category.desc,
+          meetingType: projectInfo.meetingType.desc,
+          grades: projectInfo.grades,
+          startDate: projectInfo.period.startDate,
+          endDate: projectInfo.period.endDate,
+          positions: projectInfo.positions,
+          techStack: projectInfo.skills,
+        });
+
+        setProjectDetail(projectInfo.content);
+
+        console.log(res);
+      } catch (e) {
+        console.error(e);
+      }
+    }
+    getProjectInfo();
+  }, []);
+
   return (
     <div css={wrap}>
       <JobListLinkBtn category={'프로젝트'} />
       <main css={mainContent}>
-        <ProjectInfo positions={positions} />
+        <ProjectInfo
+          meta={metaData}
+          bookMarked={bookMarkState}
+          changeBookMark={setBookMarkState}
+          summary={summary}
+          detail={projectDetail}
+        />
         <ProjectApplicationForm />
       </main>
     </div>
--- src/pages/job-detail/components/job-info/info-data/meta-items/BookMark.jsx
+++ src/pages/job-detail/components/job-info/info-data/meta-items/BookMark.jsx
@@ -1,9 +1,49 @@
-import { PiFlower } from 'react-icons/pi';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import api from '@/api/api';
 
-const BookMark = () => {
+const BookMark = ({ bookMarkState, setBookMark, recruitmentId }) => {
+  async function patchBookMarkState() {
+    const nextState = !bookMarkState.isChecked;
+
+    setBookMark((prev) => ({
+      ...prev,
+      isChecked: nextState,
+    }));
+
+    try {
+      if (nextState) {
+        const res = await api.post(`/api/v1/bookmarks/recruitments/${recruitmentId}`);
+        const newId = res.data.data;
+        console.log(res);
+        setBookMark((prev) => ({
+          ...prev,
+          id: newId,
+        }));
+      } else {
+        const res = await api.delete(`/api/v1/bookmarks/${bookMarkState.id}`);
+        console.log(res);
+
+        setBookMark((prev) => ({
+          ...prev,
+          id: -1,
+        }));
+      }
+    } catch (e) {
+      console.error(e);
+      if (e.status === 403) alert('어쩌구저쩌구');
+      else alert('실패. 다시 시도해 주세요.');
+    }
+  }
+
   return (
-    <button className="save-btn">
-      <PiFlower size={25} />
+    <button type="button" className="like-btn box-style" onClick={patchBookMarkState}>
+      {bookMarkState.isChecked ? (
+        <FavoriteIcon style={{ color: '#FA9394', fontSize: '20px', marginRight: '5px' }} />
+      ) : (
+        <FavoriteBorderIcon style={{ fontSize: '20px', marginRight: '5px' }} />
+      )}
+      찜하기
     </button>
   );
 };
