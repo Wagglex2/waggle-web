85ba1859057bbc2e0ea857451e19793614d6d4e6
JJyen <gdb1632@naver.com>
2025-12-04 04:51:39 +0900

    fix: 날짜선택 불가 버그 수정

--- src/pages/job-create/components/form/form-fields/DurationField.jsx
+++ src/pages/job-create/components/form/form-fields/DurationField.jsx
@@ -5,33 +5,23 @@ import { field, fieldContent, separator } from '../fieldStyle';
 const DurationField = () => {
   const { startDate, setStartDate, endDate, setEndDate } = useCreateJobStore();
 
-  function hadleStartDate(inputValue) {
-    if (endDate !== '' && inputValue > endDate) {
-      alert('시작일은 마감일보다 빨라야 합니다.');
-      setStartDate('');
-      return;
-    }
-
-    setStartDate(inputValue);
-  }
-
-  function handleEndDate(inputValue) {
-    if (startDate !== '' && inputValue < startDate) {
-      alert('마감일은 시작일보다 늦어야 합니다.');
-      setEndDate('');
-      return;
-    }
-
-    setEndDate(inputValue);
-  }
-
   return (
     <div css={field}>
       <p className="field-name">진행기간</p>
       <div css={fieldContent('200px')}>
-        <input type="date" value={startDate} onChange={(e) => hadleStartDate(e.target.value)} />
+        <input
+          type="date"
+          value={startDate}
+          max={endDate}
+          onChange={(e) => setStartDate(e.target.value)}
+        />
         <span css={separator}>~</span>
-        <input type="date" value={endDate} onChange={(e) => handleEndDate(e.target.value)} />
+        <input
+          type="date"
+          value={endDate}
+          min={startDate}
+          onChange={(e) => setEndDate(e.target.value)}
+        />
       </div>
     </div>
   );
