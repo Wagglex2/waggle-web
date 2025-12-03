951e3987421e593d10fa141cced17f75aebb25d4
JJyen <gdb1632@naver.com>
2025-12-04 04:55:44 +0900

    fix: 공고 상세 본문 텍스트 스타일 미적용 문제 수정

--- src/pages/job-detail/components/job-info/info-data/Details.jsx
+++ src/pages/job-detail/components/job-info/info-data/Details.jsx
@@ -16,8 +16,93 @@ const Details = ({ detail }) => {
 export default Details;
 
 const moreInfo = css`
-  margin-top: 8px;
-  white-space: pre-line;
-  font-size: 14px;
-  min-height: 100px;
+  ol {
+    padding-left: 25px;
+  }
+
+  /* 기본 스타일 */
+  white-space: pre-wrap;
+  font-size: 1em;
+  line-height: 1.5;
+  color: inherit;
+
+  /* Quill size 옵션 */
+  .ql-size-small {
+    font-size: 0.75em !important;
+  }
+  .ql-size-large {
+    font-size: 1.5em !important;
+  }
+  .ql-size-huge {
+    font-size: 2.5em !important;
+  }
+
+  /* Quill font style */
+  .ql-bold {
+    font-weight: bold !important;
+  }
+  .ql-italic {
+    font-style: italic !important;
+  }
+  .ql-underline {
+    text-decoration: underline !important;
+  }
+  .ql-strike {
+    text-decoration: line-through !important;
+  }
+
+  /* Quill alignment */
+  .ql-align-center {
+    text-align: center !important;
+  }
+  .ql-align-right {
+    text-align: right !important;
+  }
+  .ql-align-justify {
+    text-align: justify !important;
+  }
+
+  /* List 스타일 (li[data-list] 기반) */
+  li[data-list='bullet'] {
+    list-style-type: disc;
+    margin-left: 0.2em;
+    position: relative;
+  }
+  li[data-list='ordered'] {
+    list-style-type: decimal;
+    margin-left: 0.2em;
+    position: relative;
+  }
+
+  /* Quill UI span 숨기기 */
+  li[data-list] > .ql-ui {
+    display: none;
+  }
+
+  /* Indent */
+  .ql-indent-1 {
+    margin-left: 1.5em !important;
+  }
+  .ql-indent-2 {
+    margin-left: 3em !important;
+  }
+  .ql-indent-3 {
+    margin-left: 4.5em !important;
+  }
+
+  /* 링크 */
+  a {
+    color: blue;
+    text-decoration: underline;
+    cursor: pointer;
+  }
+
+  /* span 내부 상속 (inline style 유지) */
+  span {
+    font-size: inherit !important;
+    line-height: inherit;
+    font-weight: inherit;
+    font-style: inherit;
+    text-decoration: inherit;
+  }
 `;
